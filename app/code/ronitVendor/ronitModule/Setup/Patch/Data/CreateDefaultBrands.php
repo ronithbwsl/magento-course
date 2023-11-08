e53f9151c9f32a24da67b0ce2b23b1f819a52e44
Ronit Kumar Gupta <ronit.gupta@hbwsl.com>
2023-11-08 14:28:43 +0530

    Added Data Patch

---
+++ app/code/ronitVendor/ronitModule/Setup/Patch/Data/CreateDefaultBrands.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace ronitVendor\ronitModule\Setup\Patch\Data;
+
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+
+class CreateDefaultBrands implements DataPatchInterface{
+
+    private $moduleDataSetup;
+
+    public function __construct(ModuleDataSetupInterface $moduleDataSetup){
+        $this->moduleDataSetup = $moduleDataSetup;
+    }
+
+
+    public static function getDependencies(){
+        return [];
+    }
+
+    public function getAliases(){
+        return [];
+    }
+
+    public function apply(){
+        $brands = [['name'=>'Sike', 'description'=>'Something cool'],
+                    ['name'=>'Luma', 'description'=>'Something not so coll'],
+                    ['name'=>'Badidas', 'description'=>'To cool to care']
+        ];
+
+        $records = array_map(function ($brand){
+            return array_merge($brand,['is_enabled'=> 1,'website_id'=>1]);
+        }, $brands);
+
+        $this->moduleDataSetup->getConnection()->insertMultiple('ronitVendor_ronitModule',$records);
+
+        return $this;
+
+    }
+}
+
+?>
---
+++ app/code/ronitVendor/ronitModule/registration.php
@@ -0,0 +1,5 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'ronitVendor_ronitModule', __DIR__);
