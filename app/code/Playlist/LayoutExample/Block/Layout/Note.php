285b2a151fd48ba8669fb572b9eda4de22f3a2f3
Ronit Kumar Gupta <ronit.gupta@hbwsl.com>
2023-11-21 11:44:27 +0530

    Video 9:Created a child block on the fly and rendering it

--- app/code/Playlist/LayoutExample/Block/Layout/Note.php
+++ app/code/Playlist/LayoutExample/Block/Layout/Note.php
@@ -6,7 +6,7 @@ namespace Playlist\LayoutExample\Block\Layout;
 use Magento\Framework\View\Element\Template;
 
 class Note extends Template{
-
+    protected $_template = "Playlist_LayoutExample::note.phtml";
 }
 
 ?>
