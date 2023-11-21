4aec48a0d2f483e609bfbf6bfc28e3ecfc3cd152
Ronit Kumar Gupta <ronit.gupta@hbwsl.com>
2023-11-21 10:23:25 +0530

    Video 8:Added Custom Block and rendered it. removed controller configs to not rendered/override and also removed most rendering content from layout xmls. Used  to render dynamically

---
+++ app/code/Playlist/LayoutExample/Block/Layout/Index.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Playlist\LayoutExample\Block\Layout;
+use Magento\Framework\View\Element\Template;
+
+class Index extends Template{
+     
+    protected function _prepareLayout(){
+        parent::_prepareLayout();
+
+        $pageMainTitle = $this->getLayout()->getBlock('page.main.title');
+        if ($pageMainTitle) {
+            $pageMainTitle->setPageTitle("Magento Developer");
+        }
+
+        return $this;
+    }
+
+    public function getSubtitles(){
+        return "Learn Magento 2 Development";
+    }
+}
+
+
+?>
--- app/code/Playlist/LayoutExample/Controller/Layout/Index.php
+++ app/code/Playlist/LayoutExample/Controller/Layout/Index.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Playlist\LayoutExample\Controller\Layout;
 use Magento\Framework\App\ActionInterface;
 use Magento\Framework\View\Result\PageFactory;
@@ -9,10 +11,7 @@ class Index implements ActionInterface{
     public function __construct(private PageFactory $pageFactory){}
 
     public function execute(){
-        $result = $this->pageFactory->create();
-        $result->getConfig()->getTitle()->set("Ronit Kumar Gupta");
-        $result->getLayout()->getBlock("page.main.title")->setPageTitle("Magento Developer");
-        return $result;
+        return $this->pageFactory->create();
     }
 
 }
