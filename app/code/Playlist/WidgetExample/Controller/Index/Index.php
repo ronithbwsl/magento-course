e53470241c45748b03b1c1438a587b94f86568cb
Ronit Kumar Gupta <ronit.gupta@hbwsl.com>
2023-11-22 19:47:55 +0530

    Video 11: Added Widget

---
+++ app/code/Playlist/WidgetExample/Controller/Index/Index.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Playlist\WidgetExample\Controller\Index;
+use Magento\Framework\App\ActionInterface;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index implements ActionInterface{
+    protected $pageFactory;
+    public function __construct(PageFactory $pageFactory){
+        $this->pageFactory = $pageFactory;
+    }
+    public function execute(){
+        return $this->pageFactory->create();
+    }
+}
+
+?>
